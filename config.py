b931c8ffab5a99251745f5a37c1a28cb03fd3f49
Saph <s.sophi.w@gmail.com>
2021-11-24 01:07:27 -0500

    Cleaned up the reading into lists from files

--- config.py
+++ config.py
@@ -1,30 +1,26 @@
-# For variables (pantry limit, cooldown etc)
-with open('text_files/misc_text_files/updateLog.txt') as my_file:
-    updateLog = my_file.read()
-with open('text_files/misc_text_files/help.txt') as my_file:
-    helpContent = my_file.read()
-with open('text_files/misc_text_files/faq.txt') as my_file:
-    faqContent = my_file.read()
+def read_misc_file(filepath) -> str:
+    with open(filepath) as my_file:
+        file_content = my_file.read()
+    return file_content
 
-with open('text_files/bread_data/common_bread.txt') as my_file:
-    common_bread = my_file.readlines()
-    for i in range(0, len(common_bread)):
-        common_bread[i] = common_bread[i].strip()
 
-with open('text_files/bread_data/rare_bread.txt') as my_file:
-    rare_bread = my_file.readlines()
-    for i in range(0, len(rare_bread)):
-        rare_bread[i] = rare_bread[i].strip()
+def bread_file_to_list(filepath) -> list:
+    with open(filepath) as my_file:
+        bread_list = [s.strip() for s in my_file.readlines()]
+    return bread_list
 
-with open('text_files/bread_data/mythical_bread.txt') as my_file:
-    mythical_bread = my_file.readlines()
-    for i in range(0, len(mythical_bread)):
-        mythical_bread[i] = mythical_bread[i].strip()
 
-with open('text_files/bread_data/legendary_bread.txt') as my_file:
-    legendary_bread = my_file.readlines()
-    for i in range(0, len(legendary_bread)):
-        legendary_bread[i] = legendary_bread[i].strip()
+misc = ['update_log', 'help', 'faq']
+misc_paths = ['text_files/misc_text_files/updateLog.txt', 'text_files/misc_text_files/help.txt',
+              'text_files/misc_text_files/faq.txt']
+misc_to_content = {misc: read_misc_file(path) for misc, path in zip(misc, misc_paths)}
+updateLog, helpContent, faqContent = misc_to_content.values()
+
+breads = ['common', 'rare', 'mythical', 'legendary']
+bread_paths = ['text_files/bread_data/common_bread.txt', 'text_files/bread_data/rare_bread.txt',
+               'text_files/bread_data/mythical_bread.txt', 'text_files/bread_data/legendary_bread.txt']
+bread_to_list = {bread: bread_file_to_list(path) for bread, path in zip(breads, bread_paths)}
+common_bread, rare_bread, mythical_bread, legendary_bread = bread_to_list.values()
 
 pantry_limit = 1000
 bake_cooldown = 600
